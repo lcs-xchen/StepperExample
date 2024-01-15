86f24ecf130d119b1c64b57ee8713d6ae22b4b12
Cindy Chen <xchen@lcs.on.ca>
2024-01-15 11:05:39 -0500

    I have copied over the initial code

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -2,18 +2,36 @@
 //  ContentView.swift
 //  StepperExample
 //
-//  Created by Cindy Chen on 2024-01-15.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+
+                Text("5")
+                    .font(.system(size: 96))
+
+                Text("2")
+                    .font(.system(size: 44))
+
+                Text("=")
+                    .font(.system(size: 96))
+
+                Text("25")
+                    .font(.system(size: 96))
+            }
+                        
+            Spacer()
         }
         .padding()
     }
