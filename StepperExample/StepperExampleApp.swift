92c4a4e5532c239dc49a0767661a0666de220dbb
Cindy Chen <xchen@lcs.on.ca>
2024-01-15 11:03:16 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Cindy Chen on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
